c78a34251d093c032555ef6b4e1186867752a216
Soshan Wijayarathne <74235465+SoshanW@users.noreply.github.com>
2025-03-27 13:16:57 +0530

    Created basic ClienHanlder class

---
+++ group-chat-applicatoin/src/main/java/com/mycompany/group/chat/applicatoin/ClientHandler.java
@@ -0,0 +1,72 @@
+package com.mycompany.group.chat.applicatoin;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Soshan Wijayarathne
+ */
+class ClientHandler implements Runnable{
+    
+    /**
+     * The main purpose of this array is to keep track of clients, for when a 
+     * message is sent it iterates through the client list sending that one 
+     * message to all the clients
+     */
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    public Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientUserName;
+
+    public ClientHandler(Socket socket) {
+        try{
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUserName = bufferedReader.readLine();
+            clientHandlers.add(this);
+            broadCastMessage ("SERVER: " + clientUserName + " has entered the chat!");
+        }catch(IOException e){
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+    
+    
+    
+
+    @Override
+    public void run() {
+        String messageFromClient;
+        
+        while(socket.isConnected()){
+            try{
+                messageFromClient = buffereedReader.readLine();
+                broadcastMessage(messageFromClient);
+            }catch (IOException e){
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+
+    private void broadCastMessage(String messageToSend) {
+        for (ClientHandler clientHandler: clientHandlers){
+            try{
+                if (!clientHandler.clientUserName.equals(clientUserName)){
+                    clientHandler.bufferedWriter.write(messageToSend);
+                    clientHandler.bufferedWriter.newLine();
+                    clientHandler.bufferedWriter.flush();
+                }
+            }
+        }
+    }
+    
+    
+}
