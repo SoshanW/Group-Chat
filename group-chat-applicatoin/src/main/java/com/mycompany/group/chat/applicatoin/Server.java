2a85f1b3105cbf0105b04ecc2158fc37b6a88a59
Soshan Wijayarathne <74235465+SoshanW@users.noreply.github.com>
2025-03-27 12:27:18 +0530

    created basic server class

--- group-chat-applicatoin/src/main/java/com/mycompany/group/chat/applicatoin/Server.java
+++ group-chat-applicatoin/src/main/java/com/mycompany/group/chat/applicatoin/Server.java
@@ -1,5 +1,6 @@
 package com.mycompany.group.chat.applicatoin;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -26,10 +27,36 @@ public class Server {
                 
                 Socket socket = serverSocket.accept(); //This is a blocking method until a Client is connected
                 System.out.println("A new client has connected!");
-                ClientHandler clientHandler = new ClientHandler();
+                
+                                 
+                //Each object of this class will be responsible for communicating with a client
+                ClientHandler clientHandler = new ClientHandler(socket);
+                
+                Thread thread = new Thread(clientHandler);
+                thread.start();
             }
+            
+        }catch (IOException e) {
+            
         }
         
+    }
+    
+    public void closeServerSocket(){
+        try {
+            if (serverSocket != null){
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startServer();
     }
     
     
